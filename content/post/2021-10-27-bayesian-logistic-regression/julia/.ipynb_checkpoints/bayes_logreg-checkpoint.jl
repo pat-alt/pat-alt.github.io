a72d7f5eecb65a0c7367db8b2822f93016a1f8bf
pat-alt <altmeyerpat@gmail.com>
2021-11-03 16:49:55 +0100

    bleh

---
+++ content/post/2021-10-27-bayesian-logistic-regression/julia/.ipynb_checkpoints/bayes_logreg-checkpoint.jl
@@ -0,0 +1,46 @@
+# Packages
+using LinearAlgebra
+
+# Helper functions:
+∑(vector)=sum(vector)
+
+# Sigmoid function:
+function sigmoid(w,X)
+    return 1 ./ (1 .+ exp.(-X'w))
+end
+
+# Gradient:
+function ∇(w,w_0,X,y,H_0,λ)
+    N = length(y)
+    μ = sigmoid(w,X)
+    Δw = w-w_0
+    return 1/N * ∑((μ[n]-y[n])*X[n,:] for n=1:N) + H_0'Δw + 2 * λ * w
+end
+
+# Hessian:
+function ∇∇(X,y,H_0,λ)
+    N = length(y)
+    μ = sigmoid(w,X)
+    Δw = w-w_0
+    return 1/N .* ∑(μ[n] * (1-μ[n]) * X[n,:] * X[n,:]' for n=1:N) + H_0 + UniformScaling(2 * λ)
+end
+
+# Stochastic Gradient Descent:
+function sgd(X,y,∇,∇∇,w_0,ρ_0=1.0,T=1000)
+    # Initialization:
+    N = length(y)
+    # w_avg <- 1/n_iter * w # initialize average coefficients
+    t = 1 # iteration count
+    while t<=T
+        n_t = rand(1:N)
+        X_t = X[n_t,:]
+        y_t = y[n_t]
+        println(y_t)
+        # v_t = ∇(X_t,y_t,w) # compute estimate of gradient
+        # Update:
+        # w = w - eta * v_t # update coefficient vector
+        global t += 1
+    end
+    return w_0
+end
+
