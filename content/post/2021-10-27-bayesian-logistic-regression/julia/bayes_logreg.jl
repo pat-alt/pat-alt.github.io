2925643a8b305f52f77c5b6d33a96efc2749c78b
pat-alt <altmeyerpat@gmail.com>
2021-11-09 16:46:08 +0100

    charts for predictive posterior reproduced

--- content/post/2021-10-27-bayesian-logistic-regression/julia/bayes_logreg.jl
+++ content/post/2021-10-27-bayesian-logistic-regression/julia/bayes_logreg.jl
@@ -4,6 +4,18 @@ using LinearAlgebra
 # Helper functions:
 ∑(vector)=sum(vector)
 
+# Compute cartesian product over two vectors:
+function expandgrid(x,y)
+    N = length(x) * length(y)
+    grid = Iterators.product(x,y) |>
+        Iterators.flatten |>
+        collect |>
+        z -> reshape(z, (2,N)) |>
+        transpose |>
+        Matrix
+    return grid
+end
+
 # Sigmoid function:
 function sigmoid(w,X)
     trunc = 8.0 # truncation to avoid numerical over/underflow
@@ -21,22 +33,28 @@ function sigmoid(w,X)
     return p
 end
 
+# Negative log likelihood
+function nll(w,w_0,X,y,H_0)
+    N = length(y)
+    D = size(X)[2]
+    μ = sigmoid(w,X)
+    Δw = w-w_0
+    return ∑( y[n] * log(μ[n]) + (1-y[n]) * log(1-μ[n]) for n=1:N) + 1/2 * Δw'H_0*Δw
+end
+
 # Gradient:
 function ∇(w,w_0,X,y,H_0)
     N = length(y)
     μ = sigmoid(w,X)
     Δw = w-w_0
-    g = 1/N * ∑((μ[n]-y[n]) * X[n,:] for n=1:N) .+ H_0'Δw 
-    # Normalalize gradient to length 1:
-    # g = g / sqrt(g'g)
-    return g
+    return ∑((μ[n]-y[n]) * X[n,:] for n=1:N) + H_0*Δw
 end
 
 # Hessian:
 function ∇∇(X,y,w,H_0)
     N = length(y)
     μ = sigmoid(w,X)
-    return 1/N .* ∑(μ[n] * (1-μ[n]) * X[n,:] * X[n,:]' for n=1:N) + H_0 
+    return ∑(μ[n] * (1-μ[n]) * X[n,:] * X[n,:]' for n=1:N) + H_0
 end
 
 # Stochastic Gradient Descent:
@@ -62,9 +80,9 @@ struct BayesLogreg
     μ::Vector{Float64}
     Σ::Matrix{Float64}
 end
-function bayes_logreg(X,y,w_0,H_0)
+function bayes_logreg(X,y,w_0,H_0,sgd_options...)
     # Model:
-    w_map = sgd(X,y,∇,w_0,H_0) # fit the model (find mode of posterior distribution)
+    w_map = sgd(X,y,∇,w_0,H_0,sgd_options...) # fit the model (find mode of posterior distribution)
     Σ_map = inv(∇∇(X,y,w_map,H_0)) # inverse Hessian at the mode
     Σ_map = Symmetric(Σ_map) # to ensure matrix is Hermitian (i.e. avoid rounding issues)
     # Output:
@@ -84,16 +102,19 @@ end
 function posterior_predictive(mod::BayesLogreg, X)
     μ = mod.μ # MAP mean vector
     Σ = mod.Σ # MAP covariance matrix
-    κ = inv(sqrt(1 .+ π/8 .* mod.Σ)) # probit approximation
     # Inner product:
     if !isa(X, Matrix)
         if length(size(X))>1
             X = X'
         end
-        z = X'κ*μ
+        z = X'μ
     else
-        z = X*κ*μ
+        z = X*μ
     end
+    # Probit approximation
+    v = [X[n,:]'Σ*X[n,:] for n=1:size(X)[1]]
+    κ = 1 ./ sqrt.(1 .+ π/8 .* v) 
+    z = κ .* z
     # Truncation to avoid numerical over/underflow:
     trunc = 8.0 
     z = clamp.(z,-trunc,trunc)
