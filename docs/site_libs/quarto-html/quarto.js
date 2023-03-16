e97bbe82c70aa504ee92f60e0187aae3764eaa91
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-03-16 18:31:44 +0100

    post done

--- docs/site_libs/quarto-html/quarto.js
+++ docs/site_libs/quarto-html/quarto.js
@@ -614,6 +614,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
       manageTransition("TOC", slow);
       manageTransition("quarto-sidebar", slow);
     };
+
     const readerMode = !isReaderMode();
     setReaderModeValue(readerMode);
 
@@ -622,7 +623,6 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
       slowTransition(readerMode);
     }
     highlightReaderToggle(readerMode);
-    hideOverlappedSidebars();
 
     // If we're exiting reader mode, restore the non-slow transition
     if (!readerMode) {
